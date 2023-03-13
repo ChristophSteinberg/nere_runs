ef26a6b97900f8f3ffee7ce3941de92687999b79
Graf_d <120017602+ChristophSteinberg@users.noreply.github.com>
2023-03-13 15:50:15 +0100

    new2 code kaputt

--- script/script.js
+++ script/script.js
@@ -1,24 +1,26 @@
+document.body.style.overflow = "hidden";
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
-const spielfigur = [];
+let bgMusic = new Audio("sound/bg_beat.mp3");
+bgMusic.loop = true;
+// bgMusic.play();
+
+const player = [];
 const lebenImage = new Image();
-lebenImage.src = "images/idle/still1.png";
 const maxLeben = 3;
-let aktLeben = maxLeben;
-
+const jumpSound = new Audio('sound/hui.mp3');
+const moveSound = new Audio('sound/schritt.mp3');
 const hintergrund = new Image();
-hintergrund.src = "images/background_A.png";
-
 const vordergrund = new Image();
+const foregroundSpeed = 2;
+const backgroundSpeed = 2;
+
+lebenImage.src = "images/idle/still1.png";
+hintergrund.src = "images/background_A.png";
 vordergrund.src = "images/foreground.png";
 
-function zeichneHintergrund() {
-    ctx.drawImage(hintergrund, 0, 0, canvas.width, canvas.height);
-}
-
-function zeichneVordergrund() {
-    ctx.drawImage(vordergrund, 0, 0, canvas.width, canvas.height);
-}
+let scoredBarrels = 0;
+let aktLeben = maxLeben;
 let positionX = 0;
 let positionY = 350;
 let isJumping = false;
@@ -28,88 +30,265 @@ let spriteIndex = 0;
 let lastUpdate = 0;
 let isDucking = false;
 let score = 0;
-'let leben = 0;'
+let backgroundX = 0;
+let foregroundX = 0;
+
+
+function zeichneVordergrund() {
+    ctx.drawImage(vordergrund, foregroundX, 0, canvas.width, canvas.height);
+    ctx.drawImage(vordergrund, foregroundX + canvas.width, 0, canvas.width, canvas.height);
+
+    foregroundX -= foregroundSpeed;
+    if (foregroundX <= -canvas.width) {
+        foregroundX = 0;
+    }
+}
+
+function zeichneHintergrund() {
+    ctx.drawImage(hintergrund, backgroundX, 0, canvas.width, canvas.height);
+    ctx.drawImage(hintergrund, backgroundX + canvas.width, 0, canvas.width, canvas.height);
+
+    backgroundX -= backgroundSpeed;
+    if (backgroundX <= -canvas.width) {
+        backgroundX = 0;
+    }
+}
+
+function zeichneplayer() {
+    ctx.strokeStyle = "red"; // Farbe für das Rechteck
+    ctx.strokeRect(positionX, positionY, 150, 150); // Zeichnet das Rechteck um den Spieler
+
+    const now = Date.now();
+    const deltaTime = now - lastUpdate;
+    if (deltaTime > 83) {
+        spriteIndex++;
+        if (spriteIndex >= player.length) {
+            spriteIndex = 0;
+        }
+        lastUpdate = now;
+    }
+    if (isDucking) {
+        ctx.drawImage(player[spriteIndex], positionX, positionY + 70, 150, 80);
+    } else {
+        ctx.drawImage(player[spriteIndex], positionX, positionY, 150, 150);
+    }
+}
+
 class Fass {
-    constructor(x, y, image) {
+    constructor(x, y, image, fassHeight) {
         this.x = x;
         this.y = y;
         this.image = image;
-        this.width = 50;
-        this.height = 50;
-        this.speed = 5;
+        this.width = 80;
+        this.height = 80;
+        this.speed = 3;
+        this.fassHeight = fassHeight;
+        this.rotation = 0;
+        this.boundingBox = {
+            x: this.x,
+            y: this.y - fassHeight,
+            width: this.width,
+            height: this.height + fassHeight,
+        };
     }
 
     draw(ctx) {
-        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        ctx.translate(this.x + this.width / 2, this.y + this.height / 2);
+        ctx.rotate(this.rotation);
+        ctx.drawImage(this.image, -this.width / 2, -this.height / 2, this.width, this.height);
+        ctx.rotate(-this.rotation);
+        ctx.translate(-this.x - this.width / 2, -this.y - this.height / 2);
+        ctx.strokeStyle = "red"; // Farbe für das Rechteck
+        ctx.strokeRect(this.boundingBox.x, this.boundingBox.y, this.boundingBox.width, this.boundingBox.height); // Zeichnet das Rechteck
     }
+
 
     move() {
         this.x -= this.speed;
+        this.boundingBox.x = this.x;
+        this.boundingBox.y = this.y - this.fassHeight;
+        this.rotation -= 0.1;
     }
 }
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    zeichneHintergrund();
+    zeichneVordergrund();
+    zeichneplayer();
+
+    for (let i = barrels.length - 1; i >= 0; i--) {
+        const barrel = barrels[i];
+        barrel.move();
+        barrel.draw(ctx);
+
+        if (checkCollision(playerBoundingBox, barrel.boundingBox)) {
+            barrels.splice(i, 1);
+            aktLeben--;
+        }
+
+        if (barrel.x + barrel.width < 0) {
+            barrels.splice(i, 1);
+            scoredBarrels++;
+        }
+    }
+
+    if (Math.random() < 0.05) {
+        spawnFass(canvas.width, 450);
+    }
+
+    updateScore();
+    updateHealth();
+
+    requestAnimationFrame(draw);
+}
+
 const fassImage = new Image();
 fassImage.src = "images/fass.png";
 
-let fass = null;
+let barrels = [];
 
-function spawnFass() {
-    fass = new Fass(canvas.width, 450, fassImage);
-    if (fass.x - positionX < 50 && fass.x - positionX > 0 && Math.abs(fass.y - positionY) < 40) {
-        aktLeben--;
-        fass = null;
-        if (aktLeben === 0) {
-            alert("Game Over");
-            location.reload();
-        } else {
-            leben = aktLeben;
-            lebenImage.src = `images/idle/still${aktLeben}.png`;
+function spawnFass(x, y) {
+    const duckChance = Math.random();
+    const fassHeight = duckChance < 0.5 ? 50 : duckChance < 0.0 ? 100 : 160;
+    const fass = new Fass(x, y - fassHeight, fassImage, fassHeight);
+    fass.speed = 3 + Math.floor(scoredBarrels / 15);
+    barrels.push(fass);
+    score++;
+}
+
+function checkCollision(obj1, obj2) {
+    const spriteLeft = obj1.x;
+    const spriteRight = obj1.x + obj1.width;
+    const spriteTop = obj1.y;
+    const spriteBottom = obj1.y + obj1.height;
+
+    const barrelLeft = obj2.x + obj2.width;
+    const barrelRight = obj2.x + obj2.width;
+    const barrelTop = obj2.y - obj2.fassHeight;
+    const barrelBottom = obj2.y + obj2.fassheight;
+
+    return (
+        spriteLeft < barrelRight &&
+        spriteRight > barrelLeft &&
+        spriteTop < barrelBottom &&
+        spriteBottom > barrelTop
+    );
+}
+
+
+function zeichnebarrels() {
+    barrels = barrels.filter((fass) => fass.x + fass.width > 0);
+
+    let collisionDetected = false;
+    barrels.forEach((fass) => {
+        fass.move();
+        fass.draw(ctx);
+        if (fass.x + fass.width < 0) {
+            barrels.shift();
         }
+        if (checkCollision(player[0], fass.boundingBox)) {
+            console.log("Collision detected!");
+            aktLeben--;
+            if (aktLeben === 0) {
+                alert("Game Over");
+                location.reload();
+            } else {
+                lebenImage.src = `images/idle/still${aktLeben}.png`;
+            }
+            barrels.shift();
+            collisionDetected = true;
+            return;
+        } else if (fass.x - positionX < 0 && !fass.passed) {
+            fass.passed = true;
+            scoredBarrels++;
+            score = scoredBarrels;
+
+        }
+    });
+
+    if (collisionDetected) {
+        return;
     }
 }
 
+let lastFassSpawned = -1;
+const fassSpawnInterval = 2500;
+
+function gameLoop() {
+    const now = Date.now();
+    const deltaTime = now - lastUpdate;
+
+    if (isMovingLeft) {
+        bewegeLinks();
+    }
+    if (isMovingRight) {
+        bewegeRechts();
+    }
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    zeichneHintergrund();
+
+    zeichneplayer();
+    if (now - lastFassSpawned > fassSpawnInterval) {
+        lastFassSpawned = now;
+        spawnFass(1600, 480);
+    }
+    for (let i = 0; i < barrels.length; i++) {
+        if (checkCollision(player[0], barrels[i].boundingBox)) {
+            aktLeben--;
+            if (aktLeben === 0) {
+                alert("Game Over");
+                location.reload();
+            } else {
+                lebenImage.src = `images/idle/still${aktLeben}.png`;
+            }
+            barrels.splice(i, 1);
+            break;
+        }
+    }
+    zeichnebarrels();
+
+    for (let i = 0; i < aktLeben; i++) {
+        ctx.drawImage(lebenImage, 10 + i * 60, 10, 50, 50);
+    }
+    ctx.font = "30px Arial";
+    ctx.fillStyle = "white";
+    ctx.fillText(`Score: ${score}`, 500, 50);
+
+    zeichneVordergrund();
+    requestAnimationFrame(gameLoop);
+}
 
 for (let i = 1; i <= 8; i++) {
     const img = new Image();
     img.src = `images/run/run${i}.png`;
-    spielfigur.push(img);
+    player.push(img);
 }
 
 function bewegeLinks() {
+    isMovingLeft = true;
+    moveSound.play();
     positionX -= 10;
 }
 
 function bewegeRechts() {
+    isMovingRight = true;
+    moveSound.play();
     positionX += 10;
 }
 
 function springe() {
     if (!isJumping) {
         isJumping = true;
-        positionY -= 100;
+        positionY -= 120;
         setTimeout(() => {
-            positionY += 100;
+            positionY += 120;
             isJumping = false;
-        }, 300);
+        }, 400);
+        isJumping = true;
+        jumpSound.play();
     }
 }
-
-function zeichneSpielfigur() {
-    const now = Date.now();
-    const deltaTime = now - lastUpdate;
-    if (deltaTime > 83) {
-        spriteIndex++;
-        if (spriteIndex >= spielfigur.length) {
-            spriteIndex = 0;
-        }
-        lastUpdate = now;
-    }
-    if (isDucking) {
-        ctx.drawImage(spielfigur[spriteIndex], positionX, positionY + 50, 150, 100);
-    } else {
-        ctx.drawImage(spielfigur[spriteIndex], positionX, positionY, 150, 150);
-    }
-}
-
 
 document.addEventListener('keydown', (event) => {
     if (event.key === 'a') {
@@ -133,38 +312,4 @@ document.addEventListener('keyup', (event) => {
         isDucking = false;
     }
 });
-
-
-function gameLoop() {
-    if (isMovingLeft) {
-        bewegeLinks();
-    }
-    if (isMovingRight) {
-        bewegeRechts();
-    }
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    zeichneHintergrund();
-
-    zeichneSpielfigur();
-    if (fass === null) {
-        spawnFass();
-    } else {
-        fass.move();
-        fass.draw(ctx);
-        if (fass.x + fass.width < 0) {
-            fass = null;
-            score++;
-        } else if (fass.x - positionX < 50 && fass.x - positionX > 40) {
-            aktLeben--;
-            fass = null;
-        }
-        for (let i = 0; i < aktLeben; i++) {
-            ctx.drawImage(lebenImage, 10 + i * 60, 10, 50, 50);
-        }
-
-    }
-    zeichneVordergrund();
-    requestAnimationFrame(gameLoop);
-}
-
 requestAnimationFrame(gameLoop);
